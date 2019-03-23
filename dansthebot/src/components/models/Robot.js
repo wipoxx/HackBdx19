0ed50b742bbf72dc21c454745050aefb389035f1
Thomas Broutier <thomas.broutier@gmail.com>
2019-03-23 23:07:03 +0100

    Ajout des fonctions de deplacement

--- dansthebot/src/components/models/Robot.js
+++ dansthebot/src/components/models/Robot.js
@@ -1,9 +1,45 @@
-
+import Position from './Position'
 export class Robot {
-    constructor(nbBatterie, position, etat){
+    constructor(nbBatterie, position, etat, orientation){
         this.nbBatterie = nbBatterie;
         this.position = position;
+        this.orientation = orientation;
         // C'est un tableau de Malus ??
         this.etat = etat; 
     }
+
+    //Deplacements
+    MoveUp1(){
+        Move(1);
+    }
+    MoveUp3(){
+        Move(3);
+    }
+    MoveBack1(){
+        Move(-1);
+    }
+    Jump1(){
+        Move(2);
+    }
+    Move(nbCase){
+        switch(this.orientation){
+            case 'NORTH':
+                //TODO: Tester que le mouvement soit possible
+                this.position.setY(this.position.getY()-nbCase);
+            break;
+            case 'SOUTH':
+                //TODO: Tester que le mouvement soit possible
+                this.position.setY(this.position.getY()+nbCase);
+            break;
+            case 'WEST':
+                //TODO: Tester que le mouvement soit possible
+                this.position.setX(this.position.getX()-nbCase);
+            break;
+            case 'EAST':
+                //TODO: Tester que le mouvement soit possible
+                this.position.setX(this.position.getX()+nbCase);
+            break;
+        }
+    }
+
 }
