1c224758e31b1db99de1b7fbc241fdd6e2f81045
wipoxx <marine.camuset@hotmail.com>
2019-03-23 19:56:53 +0100

    create home layout with creation of players with name

--- dansthebot/src/components/layouts/HomeLayout/HomeLayout.js
+++ dansthebot/src/components/layouts/HomeLayout/HomeLayout.js
@@ -3,22 +3,56 @@ import "./HomeLayout.css";
 // import logo from "../../assets/images/logo.svg";
 
 class AboutLayout extends Component {
+	state = {
+		inputs: [{ userName: "" }],
+	};
+
+	handleChange = e => {
+		if (["userName"].includes(e.target.className)) {
+			let inputs = [...this.state.inputs];
+			inputs[e.target.dataset.id][e.target.className] = e.target.value;
+			this.setState({ inputs });
+			if (Number(e.target.dataset.id) === this.state.inputs.length - 1)
+				this.addInputField();
+		}
+	};
+
+	handleSubmit = e => {
+		e.preventDefault();
+	};
+
+	addInputField = () => {
+		this.setState(prevState => ({
+			inputs: [...prevState.inputs, { userName: "" }],
+		}));
+	};
+
 	render() {
+		const inputs = this.state.inputs;
 		return (
 			<div className="App">
 				<header className="App-header">
 					{/* <img src={logo} className="App-logo" alt="logo" /> */}
-					<p>
-						Edit <code>src/App.js</code> and save to reload.
-					</p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-					</a>
+					<h1>Welcome to Dan the bot!</h1>
+					<form onChange={this.handleChange} onSubmit={this.handleSubmit}>
+						{inputs.map((item, id) => {
+							let inputId = `input-${id}`;
+							return (
+								<div key={`div-input-${id}`}>
+									<label htmlFor={inputId}>{`Player #${id + 1}`} </label>
+									<input
+										type="text"
+										name={inputId}
+										data-id={id}
+										id={inputId}
+										defaultValue={inputs[id].userName}
+										className="userName"
+									/>
+								</div>
+							);
+						})}
+						<input type="submit" value="Submit" />
+					</form>
 				</header>
 			</div>
 		);
