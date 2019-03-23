048f19d7d82f1e1719c7570f34a5618e1d37bdbc
Thomas Broutier <thomas.broutier@gmail.com>
2019-03-23 22:57:07 +0100

    Ajout de Get et Set

--- dansthebot/src/components/models/Position.js
+++ dansthebot/src/components/models/Position.js
@@ -3,4 +3,20 @@ export class Game{
         this.x = x;
         this.y = y;
     }
+
+    getX(){
+        return this.x;
+    }
+
+    getY(){
+        return this.y;
+    }
+
+    setX(){
+        return this.x;
+    }
+
+    setY(){
+        return this.y;
+    }
 }
