78b006d93943b5ea88403bceaf6c274f53e9debf
Thomas Broutier <thomas.broutier@gmail.com>
2019-03-24 00:58:56 +0100

    Initialisation du Jeu

--- dansthebot/src/components/models/Game.js
+++ dansthebot/src/components/models/Game.js
@@ -1,6 +1,24 @@
+import { Robot } from './Robot';
+import Position from './Position'
+
 export class Game{
     constructor(nbRobot, nbTours){
+
+        //Definition de constante du jeu
+        const PosDefault = [new Position(0,0), new Position(10,0), new Position(0,10), new Position(10,10)]
+        const OrientDefault = ["SOUTH", "SOUTH", "NORTH", "NORTH"];
+
         this.nbRobot = nbRobot;
         this.nbTours = nbTours;
+        this.robots = [];
+        
+        for(var i = 0; i < nbRobot; i++) {
+            this.robots.push(new Robot(0, PosDefault[i], [], OrientDefault[i])); 
+        }
+    }
+
+    
+    doAction(robot, action){
+        robot[action];
     }
 }
