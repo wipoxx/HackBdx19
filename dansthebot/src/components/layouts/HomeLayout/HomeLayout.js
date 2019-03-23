211da58b1616e9085bf9550df8170885c88c39e2
wipoxx <marine.camuset@hotmail.com>
2019-03-23 17:51:12 +0100

    structure

---
+++ dansthebot/src/components/layouts/HomeLayout/HomeLayout.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import "./HomeLayout.css";
+// import logo from "../../assets/images/logo.svg";
+
+class AboutLayout extends Component {
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					{/* <img src={logo} className="App-logo" alt="logo" /> */}
+					<p>
+						Edit <code>src/App.js</code> and save to reload.
+					</p>
+					<a
+						className="App-link"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Learn React
+					</a>
+				</header>
+			</div>
+		);
+	}
+}
+
+export default AboutLayout;
